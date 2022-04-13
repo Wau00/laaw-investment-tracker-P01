ab48466c5de24f8650d31dfeb066807568920426
atmason90 <atmason90@gmail.com>
2022-04-13 13:02:39 -0700

    render stock data to page

--- assets/JS/stockdata.js
+++ assets/JS/stockdata.js
@@ -1,19 +1,48 @@
 // api key for stockdata.org
-var apiKey = "RamgPwgAcspYJX9SidkgGi2vtsrXoKmM2115G1fr"
+const apiKey = "RamgPwgAcspYJX9SidkgGi2vtsrXoKmM2115G1fr"
 // var tickerInput = $(".input").val().trim();
-var tickerInput = "AMD";
+let tickerInput = "AMD";
 
 
 
 function getStockData () {
-    var stockDataUrl = "https://api.stockdata.org/v1/data/quote?symbols=" + tickerInput + "&api_token=" + apiKey;
+    let stockDataUrl = "https://api.stockdata.org/v1/data/quote?symbols=" + tickerInput + "&api_token=" + apiKey;
     fetch(stockDataUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
+            let tickerName = data.data[0].name
+            let exchange = data.data[0].exchange_short
+            let price = data.data[0].price
+            let dayChange = data.data[0].day_change
+            let previousClose = data.data[0].previous_close_price
+            let openPrice = data.data[0].day_open
+            let dayHigh = data.data[0].day_high
+            let dayLow = data.data[0].day_low
+            let volume = data.data[0].volume
+            let yearHigh = data.data[0]["52_week_high"]
+            let yearLow = data.data[0]["52_week_low"]
+            let marketCap = data.data[0].market_cap
+
+            $("#tickerName").text("Company: " + tickerName);
+            $("#exchange").text("Exchange: " + exchange);
+            $("#price").text("Current Price: " + price);
+            $("#dayChange").text("Change(%)" + dayChange);
+            $("#previousClose").text("Previous Close: " + previousClose);
+            $("#openPrice").text("Open Price: " + openPrice);
+            $("#dayHigh").text("High Today: " + dayHigh);
+            $("#dayLow").text("Low Today: " + dayLow);
+            $("#volume").text("Volume: " + volume);
+            $("#yearHigh").text("52-Week High: " + yearHigh);
+            $("#yearLow").text("52-Week Low: " + yearLow);
+            $("#marketCap").text("Market Cap($): " + marketCap);
+
         })
+
+        
 }
+
 
 getStockData();
