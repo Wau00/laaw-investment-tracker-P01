8a75ab5420c6e68f94d0ad3111db8db4a49b24ad
Asika Habib <asikahabib@gmail.com>
2022-04-14 10:54:07 -0700

    added variables to JS

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -1,38 +1,7 @@
-var stocks = $("Stock Symbol");
-var watchedStocks = "";
+window.addEventListener('load', function(){
+    var stockForm = document.querySelector("#addStockForm");
+    var stockAdd = document.querySelector("#new-stock");
+    var ticketEl = document.querySelector("#ticket");
 
-var searchedStocks =(localstorage.getItem("stocks"))?JSON.parse(localStorage.getItem("stocks")):[];
-
-
-function handleAddButton (event){
-    event.preventDefault();
     
-    watchedStocks = $("Stock Symbol").val().trim();
-    if(!watchedStocks.includes(watchedStocks)){
-        searchedStocks.push(watchedStocks)
-    }
-    else{
-        return;
-    }
-    watchListAdded();
-
-    localStorage.setItem("stocks",JSON.stringify())
-
-}
-
-function watchListAdded () {
-    stocks.html("")
-    $(searchedStocks).each(function (i, el) {
-        stocks.prepend("<li>" + el + "</li>");
-    })
-    $("li").attr("class", "list-group-item list-group-item-action");
-
-    $("li").on("click", function () {
-        var listItem = $(this).text();
-        console.log($(this).text());
-        watchedStocks = listItem;
-        console.log(watchedStocks);
-    });
-};
-
 
